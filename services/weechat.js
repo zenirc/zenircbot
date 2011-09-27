b1117659c58ecabf1cfc11ef2c3803acc507ba53
Chris McDonald <XWraithanX@gmail.com>
2011-09-27 12:07:49 -0700

    adding joke weechat service I wrote.

---
+++ services/weechat.js
@@ -0,0 +1,18 @@
+var redis_lib = require('redis');
+var pub = redis_lib.createClient();
+var sub = redis_lib.createClient();
+var ticket = /.*irssi.*/;
+
+sub.subscribe('in');
+sub.on('message', function(channel, message){
+    msg = JSON.parse(message)
+    if (ticket.test(msg.message)) {
+	result = ticket.exec(msg.message)
+	console.log(result[1])
+	reply = {
+	    channel: msg.channel,
+	    message: msg.sender + ': Use weechat.',
+	}
+	pub.publish('out', JSON.stringify(reply));
+    }
+});

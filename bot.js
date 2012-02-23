35e3052350479176c6dbb1f4468d36661b4dd583
Don Park <don@donpark.org>
2012-02-23 10:44:50 +0700

    shrink the config processing a bit and get the top-level code into a method

--- bot.js
+++ bot.js
@@ -5,48 +5,15 @@ var irc = require('irc');
 var api = require('./services/lib/api');
 var config = api.load_config('bot.json');
 
-function server_options(config) {
-    return {
-	userName: config.servers[0].userName,
-	realName: config.servers[0].realName,
-	port: config.servers[0].port,
-	autoRejoin: config.servers[0].autoRejoin,
-	autoConnect: config.servers[0].autoConnect,
-	channels: config.servers[0].channels,
-	secure: config.servers[0].secure,
-	selfSigned: config.servers[0].selfSigned,
-	debug: config.options.debug,
-	floodProtection: config.options.floodProtection,
-	showErrors: config.options.debug,
-	stripColors: config.options.stripColors,
-    }
+function server_config_for(idx) {
+    var cfg = config.servers[idx]
+    /* server-generic options */
+	cfg.debug = config.options.debug;
+	cfg.floodProtection = config.options.floodProtection;
+	cfg.showErrors = config.options.debug;
+	cfg.stripColors = config.options.stripColors;
+	return cfg;
 }
-
-var bot = new irc.Client(config.servers[0].hostname, config.servers[0].nick, server_options(config));
-
-bot.addListener('message', function(nick, to, text, message) {
-    console.log(nick + ' said ' + text + ' to ' + to);
-    msg = {
-	version: 1,
-	type: 'privmsg',
-	data: {
-	    sender: nick,
-	    channel: to,
-	    message: text,
-	},
-    };
-    pub.publish('in', JSON.stringify(msg));
-});
-
-var output_handlers = {
-    1: output_version_1,
-};
-
-sub.subscribe('out')
-sub.on('message', function(channel, message) {
-    msg = JSON.parse(message);
-    output_handlers[msg.version](msg)
-});
 
 function output_version_1(message) {
     console.log('output_version_1');
@@ -61,3 +28,34 @@ function output_version_1(message) {
 	break;
     }
 }
+
+function setup() {
+	var cfg = server_config_for(0)
+	var bot = new irc.Client(cfg.hostname, cfg.nick, cfg);
+
+	bot.addListener('message', function(nick, to, text, message) {
+	    console.log(nick + ' said ' + text + ' to ' + to);
+	    msg = {
+		version: 1,
+		type: 'privmsg',
+		data: {
+		    sender: nick,
+		    channel: to,
+		    message: text,
+		},
+	    };
+	    pub.publish('in', JSON.stringify(msg));
+	});
+
+	var output_handlers = {
+	    1: output_version_1,
+	};
+
+	sub.subscribe('out')
+	sub.on('message', function(channel, message) {
+	    msg = JSON.parse(message);
+	    output_handlers[msg.version](msg)
+	});
+}
+
+setup();

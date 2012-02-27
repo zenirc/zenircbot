54d4567a29fa808caefb9bdb7fed796ade46ca21
Chris McDonald <XWraithanX@gmail.com>
2012-02-26 16:38:24 -0800

    closure passing style python, weeeee!

--- services/lib/api.py
+++ services/lib/api.py
@@ -2,6 +2,7 @@ from redis import Redis
 import json
 from threading import Thread
 
+
 def send_privmsg(to, message):
     Redis().publish('out',
                     json.dumps({
@@ -13,19 +14,30 @@ def send_privmsg(to, message):
                         }
                     }))
 
-def non_blocking_reply(service, commands):
+
+def non_blocking_redis_subscribe(func, args=[], kwargs={}):
     pubsub = Redis().pubsub()
     pubsub.subscribe('in')
     for msg in pubsub.listen():
         message = json.loads(msg['data'])
+        func(message=message, *args, **kwargs)
+
+
+def register_commands(service, commands):
+    def registration_reply(message, service, commands):
         if message['version'] == 1 and message['type'] == 'privmsg':
             if message['data']['message'] == "commands":
                 for command in commands:
-                    send_privmsg(message['data']['sender'], "%s: %s - %s" % (service, command['name'], command['description']))
+                    send_privmsg(message['data']['sender'],
+                                 "%s: %s - %s" % (service,
+                                                  command['name'],
+                                                  command['description']))
+    redis_sub = Thread(target=non_blocking_redis_subscribe,
+                       kwargs={'func': registration_reply,
+                               'kwargs': {'service': service,
+                                          'commands': commands}})
+    redis_sub.start()
 
-def register_commands(service, commands):
-    meh = Thread(target=non_blocking_reply, kwargs={'service': service, 'commands': commands})
-    meh.start()
 
 def load_config(name):
     with open(name) as f:

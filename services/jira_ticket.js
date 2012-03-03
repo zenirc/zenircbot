da8570a57125bb3056d016e050fa3e60a0b3b203
Chris McDonald <XWraithanX@gmail.com>
2012-03-03 13:26:19 -0800

    jira_ticket can now catch multiple tickets on the same line
    fixes #25

--- services/jira_ticket.js
+++ services/jira_ticket.js
@@ -1,6 +1,6 @@
 var api = require('./lib/api');
 var sub = api.get_redis_client();
-var ticket = /(?:\s|^)([a-zA-Z][a-zA-Z]-\d+)/;
+var ticket = /(?:\s|^)([a-zA-Z][a-zA-Z]-\d+)/g;
 var config = api.load_config('./jira.json');
 
 
@@ -11,11 +11,13 @@ sub.on('message', function(channel, message){
     var msg = JSON.parse(message);
     if (msg.version == 1 && msg.type == 'privmsg') {
         if (config.channels.indexOf(msg.data.channel) != -1) {
-            if (ticket.test(msg.data.message)) {
-                var result = ticket.exec(msg.data.message);
+            var result = ticket.exec(msg.data.message);
+            while (result) {
                 console.log(result[1]);
                 api.send_privmsg(config.channels,
                                  config.jira_url + 'browse/' + result[1]);
+                lastIndex = ticket.lastIndex;
+                result = ticket.exec(msg.data.message);
             }
         }
     }

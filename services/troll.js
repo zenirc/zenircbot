613c7512d3f5fddd14c3ce34dbb633fcea019d7a
Wraithan <xwraithanx@gmail.com>
2012-03-10 16:26:26 -0800

    troll.js knows about directed messages

--- services/troll.js
+++ services/troll.js
@@ -1,4 +1,5 @@
 var api = require('./lib/api');
+var redis = api.get_redis_client();
 var sub = api.get_redis_client();
 var exec = require("child_process").exec;
 
@@ -11,13 +12,23 @@ api.register_commands("troll.js", [{name: "ls",
 sub.subscribe('in');
 sub.on('message', function(channel, message){
     var msg = JSON.parse(message);
-    if (msg.version == 1 && msg.type == 'privmsg') {
-        if (/^ls$/.test(msg.data.message)) {
-            api.send_privmsg(msg.data.channel,
-                             msg.data.sender + ': http://is.gd/afolif');
-        } else if (/irssi/i.test(msg.data.message)) {
-            api.send_privmsg(msg.data.channel,
-                             msg.data.sender + ': Use weechat.');
+    if (msg.version == 1) {
+        if (msg.type == 'privmsg') {
+            if (/^ls$/.test(msg.data.message)) {
+                api.send_privmsg(msg.data.channel,
+                                 msg.data.sender + ': http://is.gd/afolif');
+            } else if (/irssi/i.test(msg.data.message)) {
+                api.send_privmsg(msg.data.channel,
+                                 msg.data.sender + ': Use weechat.');
+            }
+        } else if (msg.type == 'directed_privmsg') {
+            if (['whoareyou', 'who are you?', 'source'].indexOf(msg.data.message) != -1) {
+                redis.get('zenircbot:nick', function(err, nick) {
+                    api.send_privmsg(msg.data.channel,
+                                     'I am ' + nick + ', an instance of ZenIRCBot. ' +
+                                     'My source can be found here: https://github.com/wraithan/zenircbot');
+                });
+            }
         }
     }
 });

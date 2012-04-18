4016f43bdac1ee80889fe3450e6a22b601ca9d54
Wraithan <xwraithanx@gmail.com>
2012-04-17 19:59:37 -0700

    deprication warning

--- services/lib/api.py
+++ services/lib/api.py
@@ -1,7 +1,12 @@
 from redis import StrictRedis
 import json
 from threading import Thread
+import warnings
 
+warnings.warn(
+    'lib/api.py is deprecated, please install zenircbot_api and use that instead',
+    DeprecationWarning
+)
 
 def send_privmsg(to, message):
     if isinstance(to, basestring):

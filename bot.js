031807a44c6dbc9d6548c40e7deff52ed26b9b12
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-11-14 21:35:39 -0800

    logging and fix ping out

--- bot.js
+++ bot.js
@@ -1,35 +1,41 @@
 var irc = require('irc')
-var api = require('zenircbot-api')
-var opts = require('nomnom').option('config', {
+  , api = require('zenircbot-api')
+  , logger = require('./lib/logger')(__filename)
+  , listener = require('./lib/listener')
+  , one = require('./lib/one')
+  , opts = require('nomnom').option('config', {
                                       'abbr': 'c',
                                       'default': 'bot.json',
                                       'help': 'config file to use'
                                     }).parse()
-var listener = require('./lib/listener')
 
 var zenircbot = {
   setup: function() {
     var config = zenircbot.config = api.load_config(opts.config)
-    var zen = new api.ZenIRCBot(config.redis)
+      , zen = new api.ZenIRCBot(config.redis)
 
-    zenircbot.unsetRedisKeys(zen)
-    var serverConfig = zenircbot.server_config_for(0)
-    console.log('irc server: ' + serverConfig.hostname +
-                ' nick: '+ serverConfig.nick)
+    zenircbot.connected = false
 
-    zen.redis.set('zenircbot:nick', serverConfig.nick)
+    // propegate options to server for IRC lib
+    for (var i in config.options) {
+      config.server[i] = config.options[i]
+    }
+
+    logger.debug('irc server: ' + config.server.hostname +
+                 ' nick: '+ config.server.nick)
+
+    zen.redis.set('zenircbot:nick', config.server.nick)
     zen.redis.set('zenircbot:admin_spew_channels',
-                  serverConfig.admin_spew_channels)
+                  config.server.admin_spew_channels)
 
-    zenircbot.irc = new irc.Client(serverConfig.hostname, serverConfig.nick,
-                                   serverConfig)
-    zenircbot.pingLoop = {}
-    zenircbot.pings = {}
+    zenircbot.irc = new irc.Client(config.server.hostname
+                                  , config.server.nick
+                                  , config.server)
 
-    listener(zenircbot.irc, zen, zenircbot, serverConfig)
+    listener(zenircbot.irc, zen, zenircbot, config)
 
     var output_handlers = {
-      1: zenircbot.output_version_1
+      1: one
     }
 
     var sub = zen.get_redis_client(zenircbot.config.redis)
@@ -40,60 +46,33 @@ var zenircbot = {
     })
   },
 
-  startPing: function(config, server) {
-    zenircbot.pings[server] = 0
+  startPing: function(ping_config, hostname) {
+    zenircbot.pings = 0
 
-    zenircbot.pingLoop[server] = setInterval(function() {
-      if (zenircbot.pings[server] === config.maxFailures) {
-        console.log("Didn't receive PONG in time, reconnecting...")
-        clearInterval(zenircbot.pingLoop[server])
-        zenircbot.pings[server] = 0
-        zenircbot.pingLoop[server] = null
+    zenircbot.pingLoop = setInterval(function() {
+      if (zenircbot.pings >= ping_config.maxFailures) {
+        logger.warn('Did not receive PONG in time')
+        clearInterval(zenircbot.pingLoop)
+        zenircbot.pings = 0
+        zenircbot.pingLoop = null
         zenircbot.irc.disconnect(function(){
-          setTimeout(function(){
-            zenircbot.irc.connect()
+          zenircbot.connected = false
+          var reconnect = setInterval(function(){
+            if (!zenircbot.connected) {
+              logger.info('reconnecting...')
+              zenircbot.irc.connect()
+            } else {
+              logger.info('clearing reconnect handler...')
+              clearInterval(reconnect)
+            }
           }, 2000)
         })
       } else {
-        zenircbot.pings[server] += 1
-        console.log("Sending PING")
-        zenircbot.irc.send("PING "+server)
+        zenircbot.pings += 1
+        logger.debug('Sending PING')
+        zenircbot.irc.send('PING ' + hostname)
       }
-    }, config.frequency)
-  },
-
-  unsetRedisKeys: function(zen){
-    zen.redis.del('zenircbot:nick')
-    zen.redis.del('zenircbot:admin_spew_channels')
-  },
-
-  server_config_for: function(idx) {
-    var cfg = zenircbot.config.servers[idx]
-    /* server-generic options */
-    cfg.debug = zenircbot.config.options.debug
-    cfg.floodProtection = zenircbot.config.options.floodProtection
-    cfg.showErrors = zenircbot.config.options.debug
-    cfg.stripColors = zenircbot.config.options.stripColors
-    return cfg
-  },
-
-  output_version_1: function(bot, message) {
-    switch (message.type) {
-      case 'privmsg':
-        console.log('  privmsg: ' + JSON.stringify(message))
-        bot.say(message.data.to, message.data.message)
-        break
-
-      case 'privmsg_action':
-        console.log('  privmsg_action')
-        bot.action(message.data.to, message.data.message)
-        break
-
-      case 'raw':
-        console.log('  raw')
-        bot.send(message.command)
-        break
-    }
+    }, ping_config.frequency)
   }
 }
 
--- services/semantics.js
+++ services/semantics.js
@@ -4,36 +4,36 @@ var zen = new api.ZenIRCBot()
 zen.register_commands('semantics.js', [])
 
 function directed_message(raw_message, message, sender, channel) {
-    zen.redis.publish('in', JSON.stringify({
-        version: 1,
-        type: 'directed_privmsg',
-        data: {
-            channel: channel,
-            raw_message: raw_message,
-            message: message,
-            sender: sender
-        }
-    }))
+  zen.redis.publish('in', JSON.stringify({
+    version: 1,
+    type: 'directed_privmsg',
+    data: {
+      channel: channel,
+      raw_message: raw_message,
+      message: message,
+      sender: sender
+    }
+  }))
 }
 
 var filtered = zen.filter({version: 1, type: 'privmsg'})
 filtered.on('data', function(msg) {
-    zen.redis.get('zenircbot:nick', function(err, nick) {
-        if (!err) {
-            var parsed_message = null
-            if (msg.data.message.indexOf(nick + ': ') === 0) {
-                parsed_message = msg.data.message.substr(nick.length+2)
-            } else if (msg.data.message.indexOf('!') === 0) {
-                parsed_message = msg.data.message.substr(1)
-            } else if (msg.data.channel === msg.data.sender) {
-                parsed_message = msg.data.message
-            }
-            if (parsed_message) {
-                directed_message(msg.data.message,
-                                 parsed_message,
-                                 msg.data.sender,
-                                 msg.data.channel)
-            }
-        }
-    })
+  zen.redis.get('zenircbot:nick', function(err, nick) {
+    if (!err) {
+      var parsed_message = null
+      if (msg.data.message.indexOf(nick + ': ') === 0) {
+        parsed_message = msg.data.message.substr(nick.length+2)
+      } else if (msg.data.message.indexOf('!') === 0) {
+        parsed_message = msg.data.message.substr(1)
+      } else if (msg.data.channel === msg.data.sender) {
+        parsed_message = msg.data.message
+      }
+      if (parsed_message) {
+        directed_message(msg.data.message,
+                         parsed_message,
+                         msg.data.sender,
+                         msg.data.channel)
+      }
+    }
+  })
 })
